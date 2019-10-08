c130168d9533ae443a8e802d886b2632ab1a5117
Hederson Boechat <hederson.rocha@systax.com.br>
2019-10-08 16:29:19 -0300

    Add test for HostResolver

--- src/Proxymit.Tests/Hosts/HostResolverTests.cs
+++ src/Proxymit.Tests/Hosts/HostResolverTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +10,17 @@ namespace Proxymit.Tests.Hosts
     [TestFixture]
     public class HostResolverTests
     {
+        private Mock<HttpRequest> httpRequest;
+
+        [SetUp]
+        public void Init()
+        {
+            httpRequest = new Mock<HttpRequest>();
+            httpRequest.Setup(x => x.Host)
+                .Returns(() => new HostString("test.com"));
+
+            httpRequest.Setup(x => x.Path)
+               .Returns(() => new PathString(""));
+        }
     }
 }
