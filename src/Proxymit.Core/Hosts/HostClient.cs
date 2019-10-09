bb70be2281b1f10c41bac9ee0d9b20fc92c4f488
Hederson Boechat <hederson.rocha@systax.com.br>
2019-10-09 10:34:59 -0300

    Created HostClient to request host response

---
+++ src/Proxymit.Core/Hosts/HostClient.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Proxymit.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxymit.Core.Hosts
+{
+    public class HostClient
+    {
+        private readonly HttpClient httpClient;
+
+        public HostClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        /// <summary>
+        /// Make a request to the host and then rewrite entire response's context
+        /// </summary>
+        /// <param name="httpContext">Current http context</param>
+        /// <param name="uriHost">Host destination Uri</param>
+        /// <returns></returns>
+        public async Task Request(HttpContext httpContext, Uri uriHost)
+        {
+            var requestMessage = CreateHostRequestMessage(httpContext, uriHost);
+            using (var responseMessage =
+                await httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, httpContext.RequestAborted))
+            {
+                httpContext.Response.StatusCode = (int)responseMessage.StatusCode;
+                CopyResponseHeaders(httpContext, responseMessage);                
+
+                await responseMessage.Content.CopyToAsync(httpContext.Response.Body);
+                httpContext.Response.Body.Flush();
+                httpContext.Response.Body.Position = 0;
+            }
+        }
+
+        private HttpRequestMessage CreateHostRequestMessage(HttpContext httpContext, Uri uriHost)
+        {
+            var requestMessage = new HttpRequestMessage();
+            CopyRequestContext(httpContext, requestMessage);
+
+            requestMessage.RequestUri = uriHost;
+            requestMessage.Headers.Host = uriHost.Host;
+            requestMessage.Method = HttpUtil.GetMethod(httpContext.Request.Method);
+
+            return requestMessage;
+        }
+
+        private void CopyRequestContext(HttpContext httpContext, HttpRequestMessage httpRequestMessage)
+        {
+            var method = HttpUtil.GetMethod(httpContext.Request.Method);
+            if(method != HttpMethod.Get &&
+                method != HttpMethod.Head &&
+                method != HttpMethod.Delete &&
+                method != HttpMethod.Trace)
+            {
+                var streamContent = new StreamContent(httpContext.Request.Body);
+                httpRequestMessage.Content = streamContent;
+            }
+
+            foreach(var header in httpContext.Request.Headers)
+            {
+                httpRequestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+        }
+
+        private void CopyResponseHeaders(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            foreach(var header in responseMessage.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            foreach(var header in responseMessage.Content.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            context.Response.Headers.Remove("transfer-encoding");
+        }
+    }
+}
---
+++ src/Proxymit.Tests/Hosts/HostClientTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using Proxymit.Core.Hosts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proxymit.Tests.Hosts
+{
+    [TestFixture]
+    public class HostClientTests
+    {
+        private HttpContext httpContext;
+        private Mock<HttpMessageHandler> messageHandler;
+        private HeaderDictionary headerDictionary;
+        private HttpClient httpClient;
+        private string responseBodyString = "response works";
+
+        [SetUp]
+        public void Init()
+        {   
+
+            var responseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            responseMessage.Headers.TryAddWithoutValidation("CustomHeader", "1");
+            responseMessage.Content = new StringContent(responseBodyString);            
+            responseMessage.Content.Headers.TryAddWithoutValidation("CustomHeader2", "2");
+
+            messageHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+
+            messageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(responseMessage);
+
+            httpClient = new HttpClient(messageHandler.Object);           
+
+        }
+
+        private void SetRequestMethod(string httpMethod)
+        {
+            httpContext = new DefaultHttpContext();
+
+            var bodyStream = new MemoryStream(Encoding.ASCII.GetBytes("it's work"));
+            bodyStream.Flush();
+            bodyStream.Position = 0;
+
+            httpContext.Request.Body = bodyStream;
+            httpContext.Request.Method = httpMethod;
+
+            httpContext.Request.Headers.TryAdd("Content-Type", "text/plain");
+            httpContext.Request.Headers.TryAdd("Authentication", "Bearer 123csharp");
+
+            httpContext.Response.Body = new MemoryStream();
+
+        }
+
+        [Test]
+        [TestCase("http://test.com", "GET")]
+        [TestCase("http://test.com/api", "POST")]
+        public async Task Should_RewriteResponseContext(string url, string method)
+        {
+            var hostClient = new HostClient(httpClient);
+            SetRequestMethod(method);
+
+            await hostClient.Request(httpContext, new Uri(url));
+
+            Assert.AreEqual(3, httpContext.Response.Headers.Count);
+            
+            Assert.AreEqual("1", httpContext.Response.Headers["CustomHeader"].ToString());
+            Assert.AreEqual("2", httpContext.Response.Headers["CustomHeader2"].ToString());
+
+            using (var bodyStream = new StreamReader(httpContext.Response.Body))
+            {
+                Assert.AreEqual(responseBodyString, bodyStream.ReadToEnd());
+            }
+        }
+    }
+}
